60130f8633597059b972d5313dfa6da0a4f96d54
andrazpovse <andraz.povse1@gmail.com>
2018-11-20 18:19:53 +0100

    etcd config server. Added init-rental entry in database

--- api/src/main/java/si/nimbostratuz/bikeshare/api/v1/RentalApplication.java
+++ api/src/main/java/si/nimbostratuz/bikeshare/api/v1/RentalApplication.java
@@ -1,12 +1,10 @@
 package si.nimbostratuz.bikeshare.api.v1;
 
-import com.kumuluz.ee.discovery.annotations.RegisterService;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 
-@RegisterService
 @ApplicationPath("v1")
 public class RentalApplication extends Application {
 }
--- models/src/main/resources/sql-scripts/init-rental.sql
+++ models/src/main/resources/sql-scripts/init-rental.sql
@@ -1,3 +1,2 @@
-INSERT INTO rental (rent_start, rent_end, start_location_lat, start_location_lng,
-end_location_lat, end_location_lng, user_id, bicycle_id) VALUES (current_timestamp , current_timestamp, 99.0, 99.1, 99.2, 199.0, 1, 2);
+INSERT INTO rental (rent_start, rent_end, start_location_lat, start_location_lng,end_location_lat, end_location_lng, user_id, bicycle_id) VALUES (current_timestamp , current_timestamp, 666.0, 666.1, 666.2, 199.0, 1, 2);
 
