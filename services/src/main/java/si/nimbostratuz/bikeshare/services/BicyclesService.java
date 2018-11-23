2c439e1123a84e323fd91ab871f093dc9139319a
Matevž Fabjančič <matevz.fabjancic@gmail.com>
2018-11-23 18:33:02 +0100

    More compact methods for HTTP requests

--- services/src/main/java/si/nimbostratuz/bikeshare/services/BicyclesService.java
+++ services/src/main/java/si/nimbostratuz/bikeshare/services/BicyclesService.java
@@ -5,9 +5,6 @@ import si.nimbostratuz.bikeshare.models.dtos.BicycleDTO;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.ProcessingException;
-import javax.ws.rs.ServiceUnavailableException;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 import java.util.List;
@@ -23,27 +20,17 @@ public class BicyclesService {
     private WebTarget catalogueWebTarget;
 
     public List<BicycleDTO> getAll() {
-        try {
-            return catalogueWebTarget.path("v1")
-                                     .path("bicycles")
-                                     .request().get(new GenericType<List<BicycleDTO>>() {});
 
-        } catch (WebApplicationException | ProcessingException e) {
-            log.severe(e.getMessage());
-            throw new ServiceUnavailableException("bikeshare-catalogue not available");
-        }
+        return catalogueWebTarget.path("v1")
+                                 .path("bicycles")
+                                 .request().get(new GenericType<List<BicycleDTO>>() {});
     }
 
     public BicycleDTO getBicycle(Integer id) {
-        try {
-            return catalogueWebTarget.path("v1")
-                                     .path("bicycles")
-                                     .path(id.toString())
-                                     .request().get(new GenericType<BicycleDTO>() {});
 
-        } catch (WebApplicationException | ProcessingException e) {
-            log.severe(e.getMessage());
-            throw new ServiceUnavailableException("bikeshare-catalogue not available");
-        }
+        return catalogueWebTarget.path("v1")
+                                 .path("bicycles")
+                                 .path(id.toString())
+                                 .request().get(new GenericType<BicycleDTO>() {});
     }
 }
