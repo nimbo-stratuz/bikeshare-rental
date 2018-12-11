5975c4da6a814dc90e96f915e858788415019660
andrazpovse <andraz.povse1@gmail.com>
2018-12-11 12:34:20 +0100

    Some comments in RentalsResource.

--- api/src/main/java/si/nimbostratuz/bikeshare/api/v1/resources/RentalsResource.java
+++ api/src/main/java/si/nimbostratuz/bikeshare/api/v1/resources/RentalsResource.java
@@ -43,6 +43,9 @@ public class RentalsResource {
     }
 
     @POST
+    /*
+        Doesn't get called. Call "/rent" instead. Would do the same thing.
+     */
     public Response createRental(Rental rental) {
 
         return Response.ok(rentalsBean.create(rental)).build();
@@ -67,12 +70,19 @@ public class RentalsResource {
 
     @POST
     @Path("rent")
+    /*
+        Called when rental is initialised. Makes the bicycle unavailable etc.
+     */
     public Response rentABicycle(RentalDTO rentalDTO) {
         return Response.ok(rentalsBean.rentABicycle(rentalDTO)).build();
     }
 
     @POST
     @Path("{id}/finalize")
+    /*
+        Gets called when a rental has ended. Makes the bicycle available, sets
+        ending location etc.
+     */
     public Response finalizeRent(@PathParam("id") Integer rentalId, RentalDTO rentalDTO) {
         return Response.ok(rentalsBean.finalizeRental(rentalId, rentalDTO)).build();
 
